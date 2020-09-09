573c2067ff73d24f1004edda214bfdce5095ea96
tristan-wright <46547224+tristan-wright@users.noreply.github.com>
2020-09-09 13:58:48 +1000

    Added graph formatting

--- display.py
+++ display.py
@@ -4,16 +4,23 @@ import numpy
 if __name__ == '__main__':
     file = open('test.out', 'r')
     lines = file.readlines()
-
+    titles = {1: "Starting State Matrix", 2: "Final State Matrix", 3: "Energy vs Time", 4: "Magnetism vs Time"}
+    labels = {3: "Energy", 4: "Magnetism"}
     i = 0
     for line in lines:
-        if i < 2:
+        i += 1
+        if i < 3:
             data = numpy.matrix(line)
             plt.matshow(data)
+            plt.xlabel("x-axis")
+            plt.ylabel("y-axis")
         else:
             line = line[:-1]
             newline = line.split(' ')
+            newline = numpy.array(newline).astype(numpy.float)
             plt.plot(newline)
-        i += 1
-        plt.title(i)
+            plt.xlabel("Time")
+            plt.ylim(numpy.min(newline), numpy.max(newline))
+            plt.ylabel(labels[i])
+        plt.title(titles[i])
         plt.show()
