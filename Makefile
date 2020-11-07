3a787b4d21df7c6336eb1781a3942d3d9194bdab
tristan-wright <46547224+tristan-wright@users.noreply.github.com>
2020-11-07 22:09:47 +1000

    Commented out CUDA. Leave until last

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ FLAGS = -O
 all:
 	g++ -o ising_serial $(SOURCES) serial.cpp $(FLAGS)
 	g++ -fopenmp -o ising_omp $(SOURCES) omp.cpp $(FLAGS)
-	nvcc -o ising_cuda $(SOURCES) cuda.cu
+	#nvcc -o ising_cuda $(SOURCES) cuda.cu
 
 clean:
 	rm ising_serial ising_omp ising_cuda
--- cuda.cu
+++ cuda.cu
@@ -1,14 +1,25 @@
 #include "cuda.h"
 
+Surface* mLattice;
+
 // Error handling function from "cosc3500/cuda/example1-gpu.cu"
-void checkError(cudaError_t e)
-{
-    if (e != cudaSuccess)
-    {
+void checkError(cudaError_t e) {
+    if (e != cudaSuccess) {
         std::cerr << "CUDA error: " << int(e) << " : " << cudaGetErrorString(e) << '\n';
         abort();
     }
 }
+
+__global__
+void spins(Surface* lattice) {
+    for (int j = 0; j < lattice.size; ++j) {
+        for (int k = 0; k < lattice.size; ++k) {
+            int coords[2] = {j,k};
+            lattice.calculate_spin(coords);
+        }
+    }
+}
+
 
 /**
  */
@@ -17,12 +28,13 @@ int simulate(Surface lattice) {
         lattice.avgEnergy[i] = lattice.calculate_energy();
         lattice.avgMag[i] = lattice.calculate_magnetism();
 
-        for (int j = 0; j < lattice.size; ++j) {
-            for (int k = 0; k < lattice.size; ++k) {
-                int coords[2] = {j,k};
-                lattice.calculate_spin(coords);
-            }
-        }
+
+        checkError(cudaMalloc((void **)&mLattice, sizeof(Surface)));
+        checkError(cudaMemcpy(mLattice, lattice, sizeof(Surface), cudaMemcpyHostToDevice));
+
+        cudaFree(mLattice);
+
+        spins(&lattice);
     }
     return EXIT_SUCCESS;
 }
